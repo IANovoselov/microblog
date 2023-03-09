5c44e3e4d58a7dea0a3ef07306008774f9a63b64
ia.novoselov <ia.novoselov@tensor.ru>
2023-03-09 17:26:55 +0500

    lesson 9 finish

--- config.py
+++ config.py
@@ -8,3 +8,4 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:postgres@localhost/microblog'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    POSTS_PER_PAGE = 3
