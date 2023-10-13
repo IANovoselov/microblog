0ce8e460b98f9da08d1148839e27fff1a912ee3d
ia.novoselov <ia.novoselov@tensor.ru>
2023-10-13 13:08:58 +0500

    microblog finished

--- config.py
+++ config.py
@@ -16,8 +16,8 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
     # Настройки БД
-    #SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Пагинация
